295ef249af24094d0c1360c2976ffcb852fe0806
manavumd <manavg@umd.edu>
2025-09-20 12:53:39 +0530

    Initial Commit

---
+++ LiveActivityPOC/LiveActivityPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityPOCApp.swift
+//  LiveActivityPOC
+//
+//  Created by Manav Gupta on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveActivityPOCTests/LiveActivityPOCTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityPOCTests.swift
+//  LiveActivityPOCTests
+//
+//  Created by Manav Gupta on 20/09/25.
+//
+
+import Testing
+@testable import LiveActivityPOC
+
+struct LiveActivityPOCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
